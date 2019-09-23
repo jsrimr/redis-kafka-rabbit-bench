b9b5454ee0169ff2e3d8b18adb950f58d0155e43
acidsound <spectrick@gmail.com>
2019-09-23 16:59:57 +0900

    added: added sub

--- cloa/cloa_pub_throughput.py
+++ cloa/cloa_pub_throughput.py
@@ -16,5 +16,26 @@ def pub(ip="127.0.0.1", port=4242):
             break
     print(f"publisher throughput {cnt / 10}")
 
-if __name__ == '__main__':  
+def sub(ip="127.0.0.1", port=4242):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+    cnt = 0
+    start = datetime.datetime.now()
+
+    # subscribe a channel
+    msg = b"SUB 23482093 PP 456456456\r\n"
+    sock.send(msg)
+    data = sock.recv(13).decode('utf-8')
+    if data[:2] != "OK":
+        print("subs failed")
+        return
+    while True:
+        data = sock.recv(1024).decude('utf-8')
+        # match timestamps
+        cnt += 1
+    print(f"subscriber throughput")
+
+
+if __name__ == '__main__':
+    sub(ip="127.0.0.1", port=4242)
     pub(ip="127.0.0.1", port=4242)
