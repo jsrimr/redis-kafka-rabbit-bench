f07b910afa677a7389449c2a07c59fecdfcf34b2
jsrimr <jsrimr@naver.com>
2019-09-23 14:52:10 +0900

    Rabbit : one-file format and added argument option

---
+++ rabbitMQ/rabbit_latency_onefile.py
@@ -0,0 +1,66 @@
+import os
+from os.path import dirname
+import sys
+sys.path.append((dirname(sys.path[0])))
+from arguments import argparser
+import time
+import pika
+import multiprocessing
+from multiprocessing import Process, Queue
+
+def sub(n_msg=1000, output=None):
+    latency_list = []
+    Append = latency_list.append
+    cnt = 0
+
+    def callback(ch, method, properties, body):
+        nonlocal cnt
+        cnt += 1
+        latency = time.time() - float(body)
+        Append(latency)
+        if cnt == n_msg:
+            channel.stop_consuming()
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')  # queue 생성
+    channel.basic_consume(queue='hello', auto_ack=True,
+                          on_message_callback=callback)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+    channel.close()
+    connection.close()
+    output.put(sum(latency_list) / len(latency_list))
+
+def pub(n_msg):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')  #queue 생성
+    def pubish():
+        channel.basic_publish(exchange='',routing_key='hello',body=str(time.time()), \
+                                properties=pika.BasicProperties(timestamp=int(time.time())))
+        # print(" [x] Sent 'Hello World!'")
+    for _ in range(args.n_msg):
+        pubish()
+    channel.close() # for flushing
+    connection.close()
+
+if __name__ == '__main__':
+    args = argparser()
+
+    output = Queue()
+
+    sub_proc = Process(target=sub, args = [args.n_msg, output])
+    sub_proc.start()
+
+    pub_proc = Process(target=pub, args = [args.n_msg])
+    pub_proc.start()
+
+    # avg_latency = sub_proc.result()
+    procs = [sub_proc, pub_proc]
+    for proc in procs:
+        proc.join()
+    print(output.get())
+    output.close()
+    
--- rabbitMQ/rabbit_sub_throughput.py
+++ rabbitMQ/rabbit_sub_throughput.py
@@ -1,9 +1,32 @@
+import os
+from os.path import dirname
 import sys
+sys.path.append((dirname(sys.path[0])))
+from arguments import argparser
 import time
 import datetime
 import pika
+from multiprocessing import Process
 
-def sub(n_sec, name):
+def pub(n_sec, topic):
+    start = datetime.datetime.now()
+    cnt = 0
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue=topic)  # queue 생성
+
+    def pub_():
+        channel.basic_publish(exchange='', routing_key=topic, body=str(time.time()),
+                              properties=pika.BasicProperties(timestamp=int(time.time())))
+
+    while True:
+        pub_()
+        if datetime.datetime.now() > start + datetime.timedelta(seconds=n_sec):
+            break
+        cnt += 1
+    print(f"published {cnt / n_sec} msgs")
+
+def sub(n_sec, topic):
 
     start = datetime.datetime.now()
     sub_cnt = 0
@@ -16,17 +39,23 @@ def sub(n_sec, name):
 
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
-    channel.queue_declare(queue='hello')  # queue 생성
-    channel.basic_consume(queue='hello', auto_ack=True, on_message_callback=callback)
+    channel.queue_declare(queue=topic)  # queue 생성
+    channel.basic_consume(queue=topic, auto_ack=True, on_message_callback=callback)
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
-
-
 
     print(f"sub throughput {sub_cnt / n_sec}")
 
 
 
 if __name__ == '__main__':
-    # timer()
-    sub(10, 'reader1')
+    args = argparser()
+    sub_proc = Process(target=sub, args = [args.n_seconds,"hello"])
+    sub_proc.start()
+
+    pub_proc = Process(target=pub, args = [args.n_seconds, "hello"])
+    pub_proc.start()
+
+    procs = [sub_proc, pub_proc]
+    for proc in procs:
+        proc.join()
