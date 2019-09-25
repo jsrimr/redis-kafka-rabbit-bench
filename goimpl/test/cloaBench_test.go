9564038175e19263dead4a93050a685e8d6eac78
acidsound <spectrick@gmail.com>
2019-09-26 01:22:45 +0900

    added: pipelined cloa test

--- goimpl/test/cloaBench_test.go
+++ goimpl/test/cloaBench_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 	"errors"
+	"strings"
 )
 
 const delim = byte('\n')
@@ -158,6 +159,100 @@ func TestCloaPubSubThroughputWithMultiSubs(t *testing.T) {
 					t.Error(errors.New("pub failed"))
 				}
 				cnt++
+				nowSince := time.Since(since)
+				if nowSince > due {
+					break
+				}
+			}
+			conn.Close()
+		}(i, conn)
+	}
+	sum := 0
+	for i := 0; i < cc; i++ {
+		sum += <-cn
+	}
+	close(cn)
+	log.Println("publish throughput", sum/nSec, "msg/sec")
+	<-done
+}
+
+func TestCloaPubSubThroughputWithPipelined(t *testing.T) {
+	nSec := 1
+	cc := 100
+	pc := 10
+	cn := make(chan int, cc)
+	done := make(chan bool)
+	since := time.Now()
+	due := time.Second * time.Duration(nSec)
+	log.Println("start benchmark for cloa")
+	go func() {
+		log.Println("ready for subscribe")
+		sn := make(chan int, cc)
+		for i:=0; i<cc; i++ {
+			go func(i int) {
+				conn, err := net.Dial("tcp", "localhost:4242")
+				if err != nil {
+					t.Error(err)
+				}
+				defer conn.Close()
+				cnt := 0
+				_, err = conn.Write([]byte("SUB 23482093 PP %d\r\n"))
+				if err != nil {
+					t.Error(err)
+				}
+				buf := make([]byte, 1024)
+				for {
+					_, err = conn.Read(buf)
+					if err != nil {
+						t.Error(err)
+					}
+					cnt ++
+					nowSince := time.Since(since)
+					if nowSince> due {
+						break
+					}
+				}
+				sn <- cnt
+			}(i)
+		}
+		sum := 0
+		for i:=0; i<cc; i++ {
+			sum += <- sn
+		}
+		log.Println("subscribe throughput", sum / nSec, "msg/sec")
+		close(sn)
+		done <- true
+	}()
+
+	log.Println("ready for publish")
+	buf := make([]byte, 65536)
+	resp := []byte("OK")
+	for i := 0; i < cc; i++ {
+		conn, err := net.Dial("tcp", "localhost:4242")
+		if err != nil {
+			t.Error(err)
+		}
+		go func(i int, conn net.Conn) {
+			cnt := 0
+			defer func() {
+				cn <- cnt
+			}()
+			for {
+				/* simulates pipelining by repeated commands */
+				_, err = conn.Write([]byte(strings.Repeat(fmt.Sprintf("PUB 23482093 PP %d\r\n", time.Now().UnixNano()),pc)))
+				if err != nil {
+					t.Error(err)
+				}
+				n, err := conn.Read(buf)
+				if err != nil {
+					t.Error(err)
+				}
+				if !bytes.Equal(buf[:2], resp) {
+					log.Println("pub RESPONSE", string(buf[:n]), "-------")
+					t.Error(errors.New("pub failed"))
+				}
+				// log.Println(string(buf[:n]))
+				cnt += pc
 				nowSince := time.Since(since)
 				if nowSince > due {
 					break
