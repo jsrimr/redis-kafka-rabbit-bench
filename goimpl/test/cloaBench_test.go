b459045d3dc07ebf1cbe11c10d4e5bc4e2354c3c
acidsound <spectrick@gmail.com>
2019-09-25 19:09:57 +0900

    fixed: multiple connection /w cloa

--- goimpl/test/cloaBench_test.go
+++ goimpl/test/cloaBench_test.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net"
 	// "strconv"
+	"strings"
 	"testing"
 	"time"
 )
@@ -15,69 +16,71 @@ const delim = byte('\n')
 
 func TestCloaPubSubThroughputWithBufferedChannel(t *testing.T) {
 	nSec := 10
-	cc := 5
+	cc := 100
 	cn := make(chan int, cc)
 	done := make(chan bool)
 	since := time.Now()
 	due := time.Second * time.Duration(nSec)
 	log.Println("start benchmark for cloa")
-	/*
-		go func() {
-			conn, err := net.Dial("tcp", "localhost:4242")
+	go func() {
+		conn, err := net.Dial("tcp", "localhost:4242")
+		if err != nil {
+			t.Error(err)
+		}
+		defer conn.Close()
+		cnt := 0
+		log.Println("ready for subscribe")
+		_, err = conn.Write([]byte("SUB 23482093 PP %d\r\n"))
+		if err != nil {
+			t.Error(err)
+		}
+		buf := make([]byte, 1024)
+		for {
+			_, err = conn.Read(buf)
 			if err != nil {
 				t.Error(err)
 			}
-			defer conn.Close()
-			cnt := 0
-			log.Println("ready for subscribe")
-			_, err = conn.Write([]byte("SUB 23482093 PP %d\r\n"))
-			if err != nil {
-				t.Error(err)
+			// log.Print(n, string(buf[:n]))
+			cnt ++
+			nowSince := time.Since(since)
+			if nowSince> due {
+				break
 			}
-			buf := make([]byte, 1024)
-			sl := bufio.NewReader(conn)
-			for {
-				buf, err = sl.ReadBytes(delim)
-
-				// l, err := conn.
-				cnt ++
-				nowSince := time.Since(since)
-				if nowSince> due {
-					break
-				}
-			}
-			log.Println("subscribe throughput", cnt / nSec, "msg/sec")
-			done <- true
-		}()
-	*/
+		}
+		log.Println("subscribe throughput", cnt / nSec, "msg/sec")
+		conn.Close()
+		done <- true
+	}()
 	log.Println("ready for publish")
-	conn, err := net.Dial("tcp", "localhost:4242")
-	if err != nil {
-		t.Error(err)
-	}
 	buf := make([]byte, 1024)
 	for i := 0; i < cc; i++ {
-		go func() {
+		conn, err := net.Dial("tcp", "localhost:4242")
+		if err != nil {
+			t.Error(err)
+		}
+		go func(i int, conn net.Conn) {
 			cnt := 0
 			defer func() {
 				cn <- cnt
 			}()
 			for {
-				_, err = conn.Write([]byte(fmt.Sprintf("PUB 23482093 %d", time.Now().UnixNano())))
+				_, err = conn.Write([]byte(fmt.Sprintf("PUB 23482093 PP %d\r\n", time.Now().UnixNano())))
 				if err != nil {
 					t.Error(err)
 				}
 				_, err = conn.Read(buf)
-				// if buf[:2] == "OK" {
-				// 	t.Error(errors.New("pub failed"))
-				// }
+				if !strings.EqualFold(string(buf[:2]), "OK") {
+					// t.Error(errors.New("pub failed"))
+				}
 				cnt++
 				nowSince := time.Since(since)
 				if nowSince > due {
 					break
 				}
 			}
-		}()
+			// log.Println(i, "th concurrency is ended")
+			conn.Close()
+		}(i, conn)
 	}
 	sum := 0
 	for i := 0; i < cc; i++ {
