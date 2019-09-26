ea4ffb5878edc1ee9dcd6b8498892abe6beaf157
acidsound <spectrick@gmail.com>
2019-09-26 12:56:04 +0900

    added: process multiple response messages

--- goimpl/test/cloaBench_test.go
+++ goimpl/test/cloaBench_test.go
@@ -9,9 +9,11 @@ import (
 	"time"
 	"errors"
 	"strings"
+	"strconv"
 )
 
 const delim = byte('\n')
+var resp = []byte("OK")
 
 func TestCloaPubSubThroughputWithBufferedChannel(t *testing.T) {
 	nSec := 1
@@ -21,6 +23,11 @@ func TestCloaPubSubThroughputWithBufferedChannel(t *testing.T) {
 	since := time.Now()
 	due := time.Second * time.Duration(nSec)
 	log.Println("start benchmark for cloa")
+	log.Println("start benchmark for cloa")
+	log.Println("> publisher: ", cc)
+	log.Println("> subscribers: ", 1)
+	log.Println("> duration: ", nSec, "seconds")
+	
 	go func() {
 		conn, err := net.Dial("tcp", "localhost:4242")
 		if err != nil {
@@ -33,16 +40,30 @@ func TestCloaPubSubThroughputWithBufferedChannel(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		buf := make([]byte, 1024)
+		buf := make([]byte, 65536)
+		_, err = conn.Read(buf)
+		if err != nil {
+			t.Error(err)
+		}
+		if !bytes.Equal(buf[:2], resp) {
+			t.Error(errors.New("sub failed"))
+		}
+		READSUBS:
 		for {
-			_, err = conn.Read(buf)
+			n, err := conn.Read(buf)
 			if err != nil {
 				t.Error(err)
 			}
-			cnt ++
-			nowSince := time.Since(since)
-			if nowSince> due {
-				break
+			msgs := strings.Split(string(buf[:n]), "\r\n")
+			for _, msg := range msgs {
+				if msg == "" {
+					break
+				}
+				cnt ++
+				nowSince := time.Since(since)
+				if nowSince> due {
+					break READSUBS
+				}
 			}
 		}
 		log.Println("subscribe throughput", cnt / nSec, "msg/sec")
@@ -51,7 +72,6 @@ func TestCloaPubSubThroughputWithBufferedChannel(t *testing.T) {
 	}()
 	log.Println("ready for publish")
 	buf := make([]byte, 1024)
-	resp := []byte("OK")
 	for i := 0; i < cc; i++ {
 		conn, err := net.Dial("tcp", "localhost:4242")
 		if err != nil {
@@ -97,6 +117,10 @@ func TestCloaPubSubThroughputWithMultiSubs(t *testing.T) {
 	since := time.Now()
 	due := time.Second * time.Duration(nSec)
 	log.Println("start benchmark for cloa")
+	log.Println("> publisher: ", cc)
+	log.Println("> subscribers: ", cc)
+	log.Println("> duration: ", nSec, "seconds")
+	
 	go func() {
 		log.Println("ready for subscribe")
 		sn := make(chan int, cc)
@@ -112,16 +136,30 @@ func TestCloaPubSubThroughputWithMultiSubs(t *testing.T) {
 				if err != nil {
 					t.Error(err)
 				}
-				buf := make([]byte, 1024)
+				buf := make([]byte, 65536)
+				_, err = conn.Read(buf)
+				if err != nil {
+					t.Error(err)
+				}
+				if !bytes.Equal(buf[:2], resp) {
+					t.Error(errors.New("sub failed"))
+				}
+				READSUBS:
 				for {
-					_, err = conn.Read(buf)
+					n, err := conn.Read(buf)
 					if err != nil {
 						t.Error(err)
 					}
-					cnt ++
-					nowSince := time.Since(since)
-					if nowSince> due {
-						break
+					msgs := strings.Split(string(buf[:n]), "\r\n")
+					for _, msg := range msgs {
+						if msg == "" {
+							break
+						}
+						cnt ++
+						nowSince := time.Since(since)
+						if nowSince> due {
+							break READSUBS
+						}
 					}
 				}
 				sn <- cnt
@@ -137,8 +175,7 @@ func TestCloaPubSubThroughputWithMultiSubs(t *testing.T) {
 	}()
 
 	log.Println("ready for publish")
-	buf := make([]byte, 1024)
-	resp := []byte("OK")
+	buf := make([]byte, 65536)
 	for i := 0; i < cc; i++ {
 		conn, err := net.Dial("tcp", "localhost:4242")
 		if err != nil {
@@ -185,6 +222,10 @@ func TestCloaPubSubThroughputWithPipelined(t *testing.T) {
 	since := time.Now()
 	due := time.Second * time.Duration(nSec)
 	log.Println("start benchmark for cloa")
+	log.Println("> publishers: ", cc)
+	log.Println("> subscribers: ", cc)
+	log.Println("> duration: ", nSec, "seconds")
+
 	go func() {
 		log.Println("ready for subscribe")
 		sn := make(chan int, cc)
@@ -200,16 +241,30 @@ func TestCloaPubSubThroughputWithPipelined(t *testing.T) {
 				if err != nil {
 					t.Error(err)
 				}
-				buf := make([]byte, 1024)
+				buf := make([]byte, 65536)
+				_, err = conn.Read(buf)
+				if err != nil {
+					t.Error(err)
+				}
+				if !bytes.Equal(buf[:2], resp) {
+					t.Error(errors.New("sub failed"))
+				}
+				READSUBS:
 				for {
-					_, err = conn.Read(buf)
+					n, err := conn.Read(buf)
 					if err != nil {
 						t.Error(err)
 					}
-					cnt ++
-					nowSince := time.Since(since)
-					if nowSince> due {
-						break
+					msgs := strings.Split(string(buf[:n]), "\r\n")
+					for _, msg := range msgs {
+						if msg == "" {
+							break
+						}
+						cnt ++
+						nowSince := time.Since(since)
+						if nowSince> due {
+							break READSUBS
+						}
 					}
 				}
 				sn <- cnt
@@ -226,7 +281,6 @@ func TestCloaPubSubThroughputWithPipelined(t *testing.T) {
 
 	log.Println("ready for publish")
 	buf := make([]byte, 65536)
-	resp := []byte("OK")
 	for i := 0; i < cc; i++ {
 		conn, err := net.Dial("tcp", "localhost:4242")
 		if err != nil {
@@ -270,97 +324,100 @@ func TestCloaPubSubThroughputWithPipelined(t *testing.T) {
 	<-done
 }
 
-/*
-func TestRedisPubSubLatemncyWithBufferedChannel(t *testing.T) {
-	client := redis.NewClient(&redis.Options{Addr: "localhost:6379", Password: "", DB: 0})
-	defer client.Close()
-	nSec := 10
-	cc := 2
+func TestCloaPubSubLatencyWithBufferedChannel(t *testing.T) {
+	nSec := 1
+	cc := 100
 	cn := make(chan int, cc)
 	done := make(chan bool)
-	sub := client.Subscribe("channel")
-
 	since := time.Now()
 	due := time.Second * time.Duration(nSec)
-	log.Println("start benchmark for redis")
-	go func(sub *redis.PubSub) {
-		defer func() {
-			sub.Close()
-		}()
+	log.Println("start benchmark for cloa")
+	log.Println("> publishers: ", cc)
+	log.Println("> subscribers: ", cc)
+	log.Println("> duration: ", nSec, "seconds")
+
+	go func() {
+		conn, err := net.Dial("tcp", "localhost:4242")
+		defer conn.Close()
+		if err != nil {
+			t.Error(err)
+		}
+		defer conn.Close()
 		cnt := 0
 		latency := 0
 		log.Println("ready for subscribe")
+		_, err = conn.Write([]byte("SUB 23482093 PP %d\r\n"))
+		if err != nil {
+			t.Error(err)
+		}
+		buf := make([]byte, 65536)
+		_, err = conn.Read(buf)
+		if err != nil {
+			t.Error(err)
+		}
+		if !bytes.Equal(buf[:2], resp) {
+			t.Error(errors.New("sub failed"))
+		}
+		READSUBS:
 		for {
-			msg, _ := sub.ReceiveMessage()
-			cnt ++
-			nowSince := time.Since(since)
-			ns, _ := strconv.Atoi(msg.Payload)
-			latency += int(time.Now().UnixNano())-ns
-			if nowSince> due {
-				break
+			n, err := conn.Read(buf)
+			if err != nil {
+				t.Error(err)
+			}
+			msgs := strings.Split(string(buf[:n]), "\r\n")
+			for _, msg := range msgs {
+				if msg == "" {
+					break
+				}
+				cnt ++
+				nowSince := time.Since(since)
+				
+				ns, _ := strconv.Atoi(strings.Split(msg, " ")[3])
+				latency += int(time.Now().UnixNano())-ns
+				if nowSince> due {
+					break READSUBS
+				}
 			}
 		}
+		// TODO : calculate cuncurrent latency
 		log.Println("subscribe throughput", latency / cnt, "ns", ":cnt", cnt)
 		done <- true
-	}(sub)
+	}()
 	log.Println("ready for publish")
-	for i:=0; i<cc; i++ {
-		go func() {
-			cnt:=0
+	buf := make([]byte, 1024)
+	for i := 0; i < cc; i++ {
+		conn, err := net.Dial("tcp", "localhost:4242")
+		if err != nil {
+			t.Error(err)
+		}
+		go func(i int, conn net.Conn) {
+			cnt := 0
 			defer func() {
-				cn<-cnt
+				cn <- cnt
 			}()
 			for {
-				client.Publish("channel", strconv.FormatInt(time.Now().UnixNano(),10))
+				_, err = conn.Write([]byte(fmt.Sprintf("PUB 23482093 PP %d\r\n", time.Now().UnixNano())))
+				if err != nil {
+					t.Error(err)
+				}
+				_, err = conn.Read(buf)
+				if !bytes.Equal(buf[:2], resp) {
+					t.Error(errors.New("pub failed"))
+				}
 				cnt++
 				nowSince := time.Since(since)
-				if nowSince> due {
+				if nowSince > due {
 					break
 				}
 			}
-		}()
+			conn.Close()
+		}(i, conn)
 	}
-	sum:=0
-	for i:=0; i<cc; i++ {
+	sum := 0
+	for i := 0; i < cc; i++ {
 		sum += <-cn
 	}
 	close(cn)
-	log.Println("publish throughput", sum / nSec, "msg/sec")
-	<- done
+	log.Println("publish throughput", sum/nSec, "msg/sec")
+	<-done
 }
-
-func TestRedisSet(t *testing.T) {
-	client := redis.NewClient(&redis.Options{Addr: "localhost:6379", Password: "", DB: 0})
-	defer client.Close()
-	nSec := 10
-	cc := 5
-	cn := make(chan int, cc)
-
-	since := time.Now()
-	due := time.Second * time.Duration(nSec)
-	log.Println("start benchmark for redis")
-	log.Println("ready for set")
-	for i:=0; i<cc; i++ {
-		go func() {
-			cnt:=0
-			defer func() {
-				cn<-cnt
-			}()
-			for {
-				client.Set("k", "123456789012345678901234567989012", 0)
-				cnt++
-				nowSince := time.Since(since)
-				if nowSince> due {
-					break
-				}
-			}
-		}()
-	}
-	sum:=0
-	for i:=0; i<cc; i++ {
-		sum += <-cn
-	}
-	close(cn)
-	log.Println("publish throughput", sum / nSec, "msg/sec")
-}
-*/
